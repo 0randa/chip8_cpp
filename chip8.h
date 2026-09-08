6cd994e0e82e9c1f4a884756d20b488869617c16
Andy <andy.ruan2005@gmail.com>
2026-09-08 20:39:46 +1000

    initialise values for the chip8 struct

--- chip8.h
+++ chip8.h
@@ -1,13 +1,16 @@
+#pragma once
+
 #include <array>
 #include <cstdint>
+#include <stack>
 
 struct Chip8 {
-    std::array<uint8_t, 4096> memory; /** 4 kilobytes of memory */
-    std::array<std::array<bool, 64>, 32> display; /** a 2d array of 64 rows and 32 cols */
-    uint16_t PC; /** program counter, we use an unsigned 16 bit because want to index our memory. We don't want uint8 because it's too small, and we want an unsigned int because we don't want negatives */
-    uint16_t idx_reg; /** index register used to point to locations in memory */
+    std::array<uint8_t, 4096> memory = {}; /** 4 kilobytes of memory */
+    std::array<std::array<bool, 64>, 32> display = {}; /** 32 rows, each 64 pixels wide */
+    uint16_t PC = 0x200; /** program counter, we use an unsigned 16 bit because want to index our memory. We don't want uint8 because it's too small, and we want an unsigned int because we don't want negatives */
+    uint16_t idx_reg = 0; /** index register used to point to locations in memory */
     std::stack<uint16_t> _stack; /** A stack for 16-bit addresses, which is used to call subroutines/functions and return from them */
-    uint8_t delay_timer; /** 8 bit delay timer */
-    uint8_t sound_timer; /** 8 bit sound timer */ 
-    std::array<uint8_t, 16> general_purpose_registers; /** 16 8-bit general purpose variable registers  */
+    uint8_t delay_timer = 0; /** 8 bit delay timer */
+    uint8_t sound_timer = 0; /** 8 bit sound timer */ 
+    std::array<uint8_t, 16> general_purpose_registers = {}; /** 16 8-bit general purpose variable registers  */
 };
